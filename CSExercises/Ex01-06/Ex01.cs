fee58a321558893e6d729bb36ba39e7d6ae9a11f
Yangdashuai2015 <E0005391@u.nus.edu>
2015-08-17 22:10:50 +0800

    111

--- CSExercises/Ex01-06/Ex01.cs
+++ CSExercises/Ex01-06/Ex01.cs
@@ -10,7 +10,24 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            int N ;
+            Console.Write("please enter a interger N:\n");
+            N = Convert.ToInt32(Console.ReadLine());
+            Random X = new Random();
+            int G = X.Next(1, N);
+            Console.Write("the random num is " + G + "\n");
+            double S = Convert.ToDouble(G);
+            double J = Convert.ToDouble(N);
+            // Console.Write(S + "    " + J);
+            double S_square = S;
+            while (S_square != J)
+            {
+                S = (S + J / S) / 2;
+                S_square = Math.Round(S * S, 5);
+                S = Math.Round(S, 5);
+                Console.Write("the square root is {0:0.00000}\n", S);
+
+            };
         }
     }
 }
